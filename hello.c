88a40d01c78730a43244d44dc2a89e557355431b
Ivan Krasin <imkrasin@gmail.com>
2013-07-01 03:05:55 +0000

    Improve readability of the output

--- hello.c
+++ hello.c
@@ -6,18 +6,18 @@
 int32_t* read_counter = (int32_t*)READ_COUNTER_ADDR;
 int main(void) {
   printf("This is a test program for QEMU counter device\n");
-  printf("See http://github.com/krasin/qemu-counter for more details\n");
+  printf("See http://github.com/krasin/qemu-counter for more details\n\n");
   printf("Let's check if the Read Counter device presented\n");
   for (int i = 0; i < 10; i++) {
     printf("The device has been accessed for %"PRId32" times!\n", *read_counter);
   }
   int32_t now = *read_counter;
   if (now == 0) {
-    printf("No Read Counter detected\n");
+    printf("ERROR - No Read Counter detected\n");
   } else if (now == 11) {
-    printf("Read Counter works as intended\n");
+    printf("OK - Read Counter works as intended\n");
   } else {
-    printf("Something is wrong with Read Counter\n");
+    printf("ERROR - Something is wrong with Read Counter\n");
   }
   return 0;
 }
--- test.sh
+++ test.sh
@@ -9,8 +9,14 @@ set -ue
 
 arm-none-eabi-gcc -o hello.elf hello.c -g -mcpu=cortex-m3 -mthumb -T generic-m-hosted.ld -std=c99
 
+echo
+echo "============================================="
 echo "Running QEMU w/o read_counter device"
+echo "============================================="
 qemu-system-arm  -cpu cortex-m3 -nographic -monitor null -serial null -semihosting -kernel hello.elf
 
+echo
+echo "============================================="
 echo "Running QEMU with read_counter device"
+echo "============================================="
 qemu-system-arm  -cpu cortex-m3 -nographic -monitor null -serial null -semihosting -device read_counter -kernel hello.elf
