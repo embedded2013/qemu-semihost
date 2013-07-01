0e06294ed1435de891ca5f8ea1d342f22551dfac
Ivan Krasin <imkrasin@gmail.com>
2013-07-01 02:58:12 +0000

    Start QEMU with -device read_counter

--- test.sh
+++ test.sh
@@ -8,5 +8,9 @@ set -ue
 # Don't forget to add toolchain's /bin directory to $PATH
 
 arm-none-eabi-gcc -o hello.elf hello.c -g -mcpu=cortex-m3 -mthumb -T generic-m-hosted.ld -std=c99
+
+echo "Running QEMU w/o read_counter device"
 qemu-system-arm  -cpu cortex-m3 -nographic -monitor null -serial null -semihosting -kernel hello.elf
 
+echo "Running QEMU with read_counter device"
+qemu-system-arm  -cpu cortex-m3 -nographic -monitor null -serial null -semihosting -device read_counter -kernel hello.elf
