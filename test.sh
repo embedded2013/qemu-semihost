3b95b0d8e4c9d8dd256f1c5926c18946e33547fc
Ivan Krasin <imkrasin@gmail.com>
2013-07-01 04:31:10 +0000

    Download and build the modified qemu automatically

--- test.sh
+++ test.sh
@@ -7,6 +7,24 @@ set -ue
 # http://www.mentor.com/embedded-software/sourcery-tools/sourcery-codebench/editions/lite-edition/
 # Don't forget to add toolchain's /bin directory to $PATH
 
+# Make sure we have checked out the modified QEMU
+if [ ! -d qemu/build ]; then
+  git submodule update --init qemu
+  cd qemu
+  git submodule update --init dtc
+
+  mkdir  build
+  cd build
+  ../configure --enable-debug --target-list="arm-softmmu"
+  cd ..
+fi
+
+# Rebuild QEMU
+cd qemu/build
+make -j7
+cd ../..
+export PATH=`pwd`/qemu/build/arm-softmmu:$PATH
+
 arm-none-eabi-gcc -o hello.elf hello.c -g -mcpu=cortex-m3 -mthumb -T generic-m-hosted.ld -std=c99
 
 echo
