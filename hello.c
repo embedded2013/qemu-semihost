4e420a748b9e15b4c24dd83c817e5a26a98ce88f
Jim Huang <jserv@0xlab.org>
2013-09-25 20:31:09 +0800

    simplify counter code

--- hello.c
+++ hello.c
@@ -3,20 +3,19 @@
 
 #define READ_COUNTER_ADDR 0x40050000
 
+#define COUNTER_TIMES 5
 int32_t *read_counter = (int32_t *) READ_COUNTER_ADDR;
 int main(void)
 {
-	printf("This is a test program for QEMU counter device\n");
-	printf("See http://github.com/krasin/qemu-counter for more details\n\n");
-	printf("Let's check if the Read Counter device presented\n");
-	for (int i = 0; i < 10; i++) {
+	/* test QEMU counter device */
+	for (int i = 0; i < COUNTER_TIMES; i++) {
 		printf("The device has been accessed for %"PRId32" times!\n", *read_counter);
 	}
 	int32_t now = *read_counter;
 	if (now == 0) {
 		printf("ERROR - No Read Counter detected\n");
 	}
-	else if (now == 11) {
+	else if (now == COUNTER_TIMES + 1) {
 		printf("OK - Read Counter works as intended\n");
 	}
 	else {
